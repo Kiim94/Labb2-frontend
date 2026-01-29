30a490c4b427072464135260931d01d5c777197d
Kiim94 <nemomoa@hotmail.com>
2026-01-29 16:33:22 +0100

    Första commit

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+
+export default defineConfig({
+    base: "/Labb1-frontend/",
+    //skriv vilket plugin som används
+    plugins: [react(),
+        ViteImageOptimizer({
+            png: {quality:80},
+            jpeg:{quality:80},
+            webp:{quality: 80},
+            avif:{quality:80},
+            svg: {
+                plugins:[
+                    {name:"removeViewBox", active:false},
+                    {name: "sortAttrs"},
+                ]
+            }
+        })
+    ],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                bilder: resolve(__dirname, "bilder.html"),
+                process: resolve(__dirname, "process.html"),
+            }
+        }
+    }
+});
