9a5e070b9f9def1cf25c5908f7eadbce95fde484
Kiim94 <nemomoa@hotmail.com>
2026-01-29 17:06:47 +0100

    Följde hur Mattias gjorde, virrvarr just nu men fungerade nästan

--- src/main.js
+++ src/main.js
@@ -1,1 +1,37 @@
 import './style.css'
+
+document.addEventListener("DOMContentLoaded", async () => {
+    getCourseData();
+})
+
+async function getCourseData() {
+    const url = "https://webbutveckling.miun.se/files/ramschema.json";
+    try {
+        const response = await fetch(url);
+        const ramschema = await response.json();
+        showRamSchema(ramschema);
+    } catch (error) {
+        console.error("Något gick fel: " + error);
+    }
+}
+
+function showRamSchema(ramschema) {
+    const scheduleListEl = document.querySelector("#ramschema");
+
+    //loopa
+    ramschema.forEach(schema => {
+        scheduleListEl.innerHTML += `
+        <table>
+            <tr>
+             <th>Kurskod</th>
+             <th>Kursnamn</th>
+             <th>Progression</th>
+             <th>Kursplan</th>
+             </tr>
+             <tr>
+             <td>${schema.code}</td>
+             <td>${schema.coursename}</td>
+             <td>${schema.progression}</td>
+             <td>${schema.progression}</td>`
+    })
+}
